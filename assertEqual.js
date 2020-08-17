2f22324d62784db48e073d2ebbd601b166ecceb6
cplpearce <clint.pearce@rocketmail.com>
2020-08-17 18:49:23 +0000

    wrote first *assert* function

---
+++ assertEqual.js
@@ -0,0 +1,6 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`👍 Assertion Passed: ${actual} === ${expected}`) : console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
