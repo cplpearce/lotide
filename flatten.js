d814c4b559d094b73d914892114f23173c8b5fac
cplpearce <clint.pearce@rocketmail.com>
2020-08-18 13:00:42 -0400

    Course work, as req

---
+++ flatten.js
@@ -0,0 +1,15 @@
+/*
+flatten is a function which does something like this. Given an array with other arrays inside, it can flatten it into a single-level array.
+
+Create a function flatten which will take in an array containing elements including nested arrays of elements, and return a "flattened" version of the array.
+*/
+const unpack = function(array) {
+  unpackedArray = [];
+  
+}
+
+const flatten = function(arrayOfArrays) {
+  let flattened = true;;
+    arrayOfArrays.forEach(a => a.isArray() ? 
+  }
+};
