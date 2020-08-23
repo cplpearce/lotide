3ca715247abf166ec0c02afaeb466eea9ec56a7f
cplpearce <clint.pearce@rocketmail.com>
2020-08-23 13:23:03 -0400

    I FINALLY DID IT AND IT WORKS

--- eqObjects.js
+++ eqObjects.js
@@ -1,25 +1,12 @@
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`👍 Assertion Passed: ${actual} === ${expected}`) : console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
-};
-
-const eqObjects = function(object1, object2) {
-  for (let key in object1) {
-    if (object1[key] !== object2[key]) return false;
+const eqObjects = (a, b) => {
+  if (typeof a === 'object' && a !== null) {
+    return Object.keys(a).every(k => eqObjects(a[k], b[k]));
   }
-  for (let key in object2) {
-    if (object1[key] !== object2[key]) return false;
-  }
-  return true;
-};
+  if (a !== b) return false
+  return true
+}
 
-// test data
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-const ab2 = { a: "1", b: "2", c: "3"};
-const ba2 = { b: "2", a: "1", c: 3 };
-assertEqual(eqObjects(ab2, ba2), true); // => true
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
+console.log(eqObjects({ a: { z: 'cat' }, b: 3 }, { a: { z: 'cat' }, b: 3 })) // => true
