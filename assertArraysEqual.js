cbefa3ec04b5f7b3df0c5e6de30e06ba3f01302a
cplpearce <clint.pearce@rocketmail.com>
2020-08-24 13:50:57 -0400

    Cosmetic changes, emojis, etc

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,9 +2,9 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected) === true) {
-    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+    console.log(`👍 Assertion Passed: "${actual}" === "${expected}"`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+    console.log(`🛑 Assertion Failed: "${actual}" !== "${expected}"`);
   }
 };
 
--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`👍 Assertion Passed: ${actual} === ${expected}`) : console.log(`⛔ Assertion Failed: ${actual} !== ${expected}`);
+  actual === expected ? console.log(`👍 Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
 module.exports = assertEqual;
